0a86e05519f9d353bfee17dfc0c0420c90361c7a
Hippolyte Thomas <contact@hippolyte-thomas.fr>
2022-11-21 15:27:47 +0100

    Add user & files to fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use Faker\Factory;
+use App\Entity\User;
 use App\Entity\Media;
 use App\Entity\Social;
 use App\Entity\AboutMe;
@@ -10,16 +11,33 @@ use App\Entity\Project;
 use App\Entity\Technology;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private $hasher;
+
+    public function __construct (UserPasswordHasherInterface $userPasswordHasherInterface) 
+    {
+        $this->hasher = $userPasswordHasherInterface;
+    }
+
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('FR-fr');
 
+        // Random picture only used for tests purpose
+        $test_pictures = [
+            'tests/picture1.jpg',
+            'tests/picture2.jpg',
+            'tests/picture3.jpg',
+            'tests/picture4.jpg',
+            'tests/picture5.jpg',
+        ];
+
         // Adding profile picture for AboutMe
         $picture = new Media();
-        $picture->setUrl('https://randomuser.me/api/portraits/men/'.mt_rand(1, 99).'.jpg')
+        $picture->setFileName($test_pictures[mt_rand(0, count($test_pictures)-1)])
                 ->setCaption($faker->sentence(10));
 
         $manager->persist($picture);
@@ -36,7 +54,7 @@ class AppFixtures extends Fixture
         // Generating Socials
         for ($i=0; $i < 3; $i++) { 
             $picture = new Media();
-            $picture->setUrl('https://picsum.photos/id/'.mt_rand(1, 99).'/32/32');
+            $picture->setFileName($test_pictures[mt_rand(0, count($test_pictures)-1)]);
 
             $manager->persist($picture);
             
@@ -73,7 +91,7 @@ class AppFixtures extends Fixture
             // Adding random pictures
             for ($j=1; $j <= mt_rand(1, 4); $j++) {
                 $picture = new Media();
-                $picture->setUrl('https://picsum.photos/id/'.mt_rand(1, 99).'/850/500')
+                $picture->setFileName($test_pictures[mt_rand(0, count($test_pictures)-1)])
                         ->setCaption($faker->sentence(10));
                 $manager->persist($picture);
 
@@ -82,6 +100,18 @@ class AppFixtures extends Fixture
 
             $manager->persist($project);
         }
+
+        // Generating Admin User
+        $user = new User();
+        $password = $this->hasher->hashPassword(
+            $user, "password"
+        );
+
+        $user->setUsername("admin")
+             ->setRoles(['ROLE_ADMIN'])
+             ->setPassword($password);
+
+        $manager->persist($user);
 
         $manager->flush();
     }
