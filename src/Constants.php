82edcd1fdab6223dfbd61ef5077140190f93f315
Hippolyte Thomas <contact@hippolyte-thomas.fr>
2024-01-31 11:56:14 +1300

    feat: add constants & app version

---
+++ src/Constants.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+class Constants
+{
+    /**
+     * The current release version
+	 * Follows the Semantic Versioning strategy: https://semver.org/
+     */
+    public const string VERSION = '1.1.0';
+    /**
+     * The current release: major * 10000 + minor * 100 + patch
+     */
+    public const int VERSION_ID = 10100;
+}
