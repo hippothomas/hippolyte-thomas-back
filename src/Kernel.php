a21c5c900715482ff5df284942c669744922da4b
Hippolyte Thomas <contact@hippolyte-thomas.fr>
2024-01-31 21:35:35 +1300

    fix(doctrine): User Deprecated: Relying on non-optimal defaults for ID generation
    https://github.com/doctrine/orm/issues/8893

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,10 +2,30 @@
 
 namespace App;
 
+use Doctrine\DBAL\Platforms\PostgreSQLPlatform;
+use Doctrine\ORM\Mapping\ClassMetadataInfo;
+use Override;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    #[Override]
+    protected function build(ContainerBuilder $container): void
+    {
+        // fixing issue: https://github.com/doctrine/orm/issues/8893
+        $container->addCompilerPass(new class() implements CompilerPassInterface {
+            public function process(ContainerBuilder $container): void
+            {
+                $container->getDefinition('doctrine.orm.default_configuration')
+                    ->addMethodCall('setIdentityGenerationPreferences', [
+                        [ PostgreSQLPlatform::class => ClassMetadataInfo::GENERATOR_TYPE_SEQUENCE ],
+                    ]);
+            }
+        });
+    }
 }
