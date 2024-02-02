df32c2b046c55831e142183e8bed0806da9295d5
Hippolyte Thomas <contact@hippolyte-thomas.fr>
2024-02-02 13:41:56 +1300

    feat: edit PHPCSFixer configuration

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -3,11 +3,14 @@
 $finder = (new PhpCsFixer\Finder())
     ->in(__DIR__)
     ->exclude('var')
+    ->exclude('vendor')
 ;
 
 return (new PhpCsFixer\Config())
     ->setRules([
+        '@PSR12' => true,
         '@Symfony' => true,
+        '@DoctrineAnnotation' => true,
     ])
     ->setFinder($finder)
 ;
