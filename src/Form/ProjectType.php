e96158d18764429f47ef5c825f0cc5b1feaf9bd5
Hippolyte Thomas <contact@hippolyte-thomas.fr>
2024-02-19 17:31:53 +1300

    feat: removing ckeditor - bundle is deprecated - stuck w/ last version before LTS (free user)

--- src/Form/ProjectType.php
+++ src/Form/ProjectType.php
@@ -4,9 +4,9 @@ namespace App\Form;
 
 use App\Entity\Project;
 use App\Entity\Technology;
-use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -19,7 +19,7 @@ class ProjectType extends ApplicationType
             ->add('name', TextType::class, $this->getConfiguration('Nom', 'Nom du projet'))
             ->add('slug', TextType::class, $this->getConfiguration('Slug', 'Chaine URL du projet (auto)', ['required' => false]))
             ->add('introduction', TextType::class, $this->getConfiguration('Introduction', 'Description courte du projet (1 phrase)'))
-            ->add('description', CKEditorType::class, $this->getConfiguration('Description', 'Description détaillée du projet'))
+            ->add('description', TextareaType::class, $this->getConfiguration('Description', 'Description détaillée du projet'))
             ->add('technologies', EntityType::class, [
                 'label' => 'Technologies',
                 'class' => Technology::class,
