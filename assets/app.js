4daa5995c886a780d4e1037f0aff0e8f3208436c
Hippolyte Thomas <contact@hippolyte-thomas.fr>
2024-02-20 08:38:09 +1300

    feat(front): handle assets & libs with webpack

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,14 @@
 // Style
 // ==================
 import './scss/app.scss';
+
+// Imports
+// ==================
+import { loadScripts } from './js/functions';
+import { pictures } from './js/pictures';
+import { project } from './js/project';
+
+// Load Scripts
+// ==================
+loadScripts('project', project);
+loadScripts('edit-btn', pictures);
