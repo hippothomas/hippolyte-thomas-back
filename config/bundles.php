b32872598466c1bfb18da0a8f5fbbaf0be52046b
Hippolyte Thomas <contact@hippolyte-thomas.fr>
2024-03-01 11:40:05 +1300

    feat: add paginator, edit menu, translation config & twig globals

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     HWI\Bundle\OAuthBundle\HWIOAuthBundle::class => ['all' => true],
     KevinPapst\TablerBundle\TablerBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
