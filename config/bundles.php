be5517d1c7abe4f2f90afdad018472adae0d5d1c
Hippolyte Thomas <contact@hippolyte-thomas.fr>
2024-03-02 11:36:17 +1300

    feat(tests): add configuration

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     KevinPapst\TablerBundle\TablerBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];
