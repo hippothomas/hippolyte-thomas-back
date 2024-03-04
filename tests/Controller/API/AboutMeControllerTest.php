f77483ee4ca8c7605797658b20777ec4ea0b806b
Hippolyte Thomas <contact@hippolyte-thomas.fr>
2024-03-04 15:20:43 +1300

    feat(tests): remove API_KEY const from AboutMeControllerTest

--- tests/Controller/API/AboutMeControllerTest.php
+++ tests/Controller/API/AboutMeControllerTest.php
@@ -7,8 +7,6 @@ use App\Tests\ApiTestCase;
 
 class AboutMeControllerTest extends ApiTestCase
 {
-    public const string API_KEY = '?api_key=00000000-0000-0000-0000-000000000000';
-
     public function testAboutMe(): void
     {
         $client = static::createClient();
