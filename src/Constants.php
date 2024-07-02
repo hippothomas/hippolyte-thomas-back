9fb2c401365cf9542a896c9912f7e090d6335d3e
Hippolyte Thomas <contact@hippolyte-thomas.fr>
2024-07-03 11:29:37 +1200

    fix: update release version

--- src/Constants.php
+++ src/Constants.php
@@ -8,11 +8,11 @@ class Constants
      * The current release version
      * Follows the Semantic Versioning strategy: https://semver.org/.
      */
-    public const string VERSION = '2.0.3';
+    public const string VERSION = '2.0.4';
     /**
      * The current release: major * 10000 + minor * 100 + patch.
      */
-    public const int VERSION_ID = 20003;
+    public const int VERSION_ID = 20004;
     /**
      * Main Website URL.
      */
